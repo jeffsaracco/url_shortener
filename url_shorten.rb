fdc498d3f742cd1504a6b962677f3521eaad2bc2
Jeff Saracco <jeffsaracco@gmail.com>
2012-07-30 19:54:55 -0400

    Did a lot of stuff, it works, its great :)

--- url_shorten.rb
+++ url_shorten.rb
@@ -1,4 +1,16 @@
 require 'sinatra'
+require 'sqlite3'
+
+# class ShortenedURL
+  # #initialize
+  # #save - to the db
+  # #get stored url
+  # #generate url
+  # #list of urls generated so far
+  # #find by shortened url
+# end
+
+db = SQLite3::Database.new('url.db')
 
 get '/new' do
   "<html>
@@ -16,5 +28,12 @@ post '/new' do
   6.times do 
     new_url << ('A'..'Z').to_a.sample
   end
+  db.execute("insert into urls values (?, ?)", params[:url], new_url.join)
   "Your new URL is #{new_url.join} which redirects to #{params["url"]}"
+end
+
+get '/:short_url' do |url|
+  result = db.execute("select url from urls where shortened_url = ?", url)
+  destination_url = result[0][0]
+  redirect destination_url
 end
